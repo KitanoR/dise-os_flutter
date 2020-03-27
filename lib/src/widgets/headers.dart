40347dc6d7c6bde9622675c9b02e3c76c19a0d1a
Cayetano Rosales <cayeroza@gmail.com>
2020-03-26 22:02:43 -0600

    Agregando header Waves

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -208,3 +208,49 @@ class _HeaderCurvaPainter extends CustomPainter {
   }
 
 }
+
+class HeaderWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    // propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 20;
+    
+    final path = new Path();
+
+    // dibujar con el path y el lapiz
+    path.lineTo(0, size.height * .25);
+    path.quadraticBezierTo(size.width *0.25, size.height * 0.30, size.width * 0.5, size.height * .25);
+    path.quadraticBezierTo(size.width *0.72, size.height * 0.20, size.width, size.height * .25);
+    path.lineTo(size.width, 0);
+    //path.lineTo(size.width, size.height * .25);
+   
+
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+
+}
